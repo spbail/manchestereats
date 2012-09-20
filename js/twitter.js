0b6c1dd5ae66b1ff560e6c8e564a8d7fa3cc4190
sbail <sbail@delorean>
2012-09-20 23:20:51 +0100

    first commit

---
+++ js/twitter.js
@@ -0,0 +1,187 @@
+JQTWEET = {
+	
+	// Set twitter username, number of tweets & id/class to append tweets
+	hash:'%23mcreats OR %23manchestereats',
+	appendTo: '#jstwitter',
+	imgWidth: 220,
+
+	// core function of jqtweet
+	loadTweets: function() {
+		$.ajax({
+			url: 'http://search.twitter.com/search.json',
+			dataType: 'jsonp',
+			data: {
+				q: JQTWEET.hash,
+				include_entities: true
+			},
+			success: function(data, textStatus, xhr) {
+			    data = data['results'];
+
+                 var tweet_box = '<div class="tweet">IMG_TAG';
+                 tweet_box+= '<div class="tweet-user">@USER: </div>';
+                 tweet_box+= '<span class="tweet-text">TWEET_TEXT</span>';
+                 tweet_box+= '<div class="time"><i class="icon-time"></i> <span class="time-text">AGO</span></div>';
+
+                 var img;
+				 // append tweets into page
+				 for (var i = 0; i < data.length; i++) {
+					try {
+					    var username = data[i].from_user;
+						if (data[i].entities.media) {
+							img = '<a href="' + data[i].entities.media[0].media_url + ':large" class="fancy">';
+							img += '<img src="' + data[i].entities.media[0].media_url + ':small" alt=""/>';
+							img += '</a>';
+						} else {
+							img = '';
+						}
+					
+					} catch (e) {
+					
+					}
+
+					$(JQTWEET.appendTo).append(
+						tweet_box.replace('IMG_TAG', img)
+							.replace('TWEET_TEXT', JQTWEET.ify.clean(data[i].text, img) )
+							.replace(/USER/g, '<a href="http://www.twitter.com/' + username + '">' + username + '</a>')
+							.replace('AGO', JQTWEET.timeAgo(data[i].created_at) )
+							.replace(/ID/g, data[i].id_str)							
+					);								
+				 }
+				
+				//trigger jQuery Masonry once all data are loaded				
+				var $container = $('#jstwitter');
+				$container.imagesLoaded(function(){
+				  $container.masonry({
+				    itemSelector : '.tweet',
+				    columnWidth : 0,
+				    isAnimated: true
+				  });
+				});		
+				
+				//the last step, activate fancybox 
+				$("a.fancy").fancybox({
+					'overlayShow'	: false,
+					'transitionIn'	: 'elastic',
+					'transitionOut'	: 'elastic',
+					'overlayShow'	: true
+				});							 					
+				
+			}	
+
+		});
+		
+	}, 
+	
+		
+	/**
+      * relative time calculator FROM TWITTER
+      * @param {string} twitter date string returned from Twitter API
+      * @return {string} relative time like "2 minutes ago"
+      */
+    timeAgo: function(dateString) {
+		var rightNow = new Date();
+		var then = new Date(dateString);
+		
+		if ($.browser.msie) {
+			// IE can't parse these crazy Ruby dates
+			then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
+		}
+
+		var diff = rightNow - then;
+
+		var second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24,
+		week = day * 7;
+
+		if (isNaN(diff) || diff < 0) {
+			return ""; // return blank string if unknown
+		}
+
+		if (diff < second * 2) {
+			// within 2 seconds
+			return "right now";
+		}
+
+		if (diff < minute) {
+			return Math.floor(diff / second) + " seconds ago";
+		}
+
+		if (diff < minute * 2) {
+			return "about 1 minute ago";
+		}
+
+		if (diff < hour) {
+			return Math.floor(diff / minute) + " minutes ago";
+		}
+
+		if (diff < hour * 2) {
+			return "about 1 hour ago";
+		}
+
+		if (diff < day) {
+			return  Math.floor(diff / hour) + " hours ago";
+		}
+
+		if (diff > day && diff < day * 2) {
+			return "yesterday";
+		}
+
+		if (diff < day * 365) {
+			return Math.floor(diff / day) + " days ago";
+		}
+
+		else {
+			return "over a year ago";
+		}
+	}, // timeAgo()
+    
+	
+    /**
+      * The Twitalinkahashifyer!
+      * http://www.dustindiaz.com/basement/ify.html
+      * Eg:
+      * ify.clean('your tweet text');
+      */
+    ify:  {
+      link: function(tweet, hasIMG) {
+        return tweet.replace(/\b(((https*\:\/\/)|www\.)[^\"\']+?)(([!?,.\)]+)?(\s|$))/g, function(link, m1, m2, m3, m4) {
+          var http = m2.match(/w/) ? 'http://' : '';
+          if (hasIMG) return '';
+          else return '<a class="twtr-hyperlink" target="_blank" href="' + http + m1 + '">' + ((m1.length > 25) ? m1.substr(0, 24) + '...' : m1) + '</a>' + m4;
+        });
+      },
+
+      at: function(tweet) {
+        return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20})/g, function(m, username) {
+          return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/intent/user?screen_name=' + username + '">@' + username + '</a>';
+        });
+      },
+
+      list: function(tweet) {
+        return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20}\/\w+)/g, function(m, userlist) {
+          return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/' + userlist + '">@' + userlist + '</a>';
+        });
+      },
+
+      hash: function(tweet) {
+        return tweet.replace(/(^|\s+)#(\w+)/gi, function(m, before, hash) {
+          return before + '<a target="_blank" class="twtr-hashtag" href="http://twitter.com/search?q=%23' + hash + '">#' + hash + '</a>';
+        });
+      },
+
+      clean: function(tweet , hasIMG) {
+	      return this.hash(this.at(this.list(this.link(tweet, hasIMG))));
+
+      }
+    } // ify
+
+	
+};
+
+
+
+
+// start jqtweet!
+JQTWEET.loadTweets();
